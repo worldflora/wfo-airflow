8f0636fe6127608ee432697c1153c625cfb7f6f5
Roger <rhyam@rbge.org.uk>
2026-04-29 17:09:29 +0100

    now with file storage

--- graphs/portal_index_facets_metadata.py
+++ graphs/portal_index_facets_metadata.py
@@ -4,6 +4,7 @@ from airflow.sdk import dag, task, Variable, Param
 from datetime import timedelta
 from airflow.sdk.exceptions import AirflowFailException
 import os
+import re
 import json
 from includes.portal_api import PortalApi
 from includes.fyllo_api import FylloApi
@@ -29,6 +30,17 @@ def portal_index_facets_metadata():
     """
     
     @task()
+    def setup_local_storage(**context):
+        base = Variable.get("local-file-storage")
+        subdir = base + "/portal_index_facets_metadata/"
+        if not os.path.exists(subdir):
+            os.makedirs(subdir)
+        safe_file_name = re.sub(r"[/\\?%*:|\"<>\x7F\x00-\x1F]", "-", context["run_id"])
+        file_path = subdir + safe_file_name
+        print(file_path)
+        context["ti"].xcom_push(key="file_path", value=file_path)
+
+    @task()
     def fetch_last_modified_from_portal(**context):
 
         # if flagged to index all we simple index from the start of the epoc
@@ -45,24 +57,41 @@ def portal_index_facets_metadata():
 
     @task()
     def fetch_metadata_from_fyllo(**context):
+
+        # get the metadata
         fyllo = FylloApi(Variable.get("fyllo-api-url"), Variable.get("fyllo-api-token"))
         since = context["ti"].xcom_pull(task_ids="fetch_last_modified_from_portal", key="since")
         metadata = fyllo.fetchFacetMetadata(since)
         print(f"Metadata documents fetched: {len(metadata['docs'])}")
-        context["ti"].xcom_push(key="metadata", value=metadata)
-        return metadata
-    
+
+        # write it to a file
+        file_path = context["ti"].xcom_pull(task_ids="setup_local_storage", key="file_path")
+        with open(file_path, "w") as f:
+            json.dump(metadata, f)
+        
     @task()
     def push_metadata_to_portal(**context):
-        portal = PortalApi(Variable.get("portal-api-url"), Variable.get("portal-api-token"))
-        metadata = context["ti"].xcom_pull(task_ids="fetch_metadata_from_fyllo", key="metadata")
-        response = portal.pushFacetMetadata(metadata)
-        if not response['success']: 
-            print(response['message'])
-            raise AirflowFailException("Failed to save metadata to index")
+
+        # read the metadata from the file
+        file_path = context["ti"].xcom_pull(task_ids="setup_local_storage", key="file_path")
+        with open(file_path, "r") as f:
+            metadata = json.load(f)
+
+            # post it to the portal
+            portal = PortalApi(Variable.get("portal-api-url"), Variable.get("portal-api-token"))
+            response = portal.pushFacetMetadata(metadata)
+            if not response['success']: 
+                print(response['message'])
+                raise AirflowFailException("Failed to save metadata to index")
+    
+    @task()
+    def tear_down_local_storage(**context):
+        file_path = context["ti"].xcom_pull(task_ids="setup_local_storage", key="file_path")
+        if os.path.exists(file_path):
+            os.remove(file_path)
             
     # dag wiring diagram
-    fetch_last_modified_from_portal() >> fetch_metadata_from_fyllo() >>  push_metadata_to_portal()
+    setup_local_storage() >> fetch_last_modified_from_portal() >> fetch_metadata_from_fyllo() >>  push_metadata_to_portal() >> tear_down_local_storage()
 
 portal_index_facets_metadata()
 
